bd0469b0a300d14cfb08da51dae66fb0d79eaee1
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-09-11 20:59:57 -0500

    footer completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/shared/header/header.component';
+import { FooterComponent } from './componentes/shared/footer/footer.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { LoginComponent } from './componentes/user/login/login.component';
+import { RegisterComponent } from './componentes/user/register/register.component';
+import { ProfileComponent } from './componentes/user/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ProductosComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
